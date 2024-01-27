3eeb133985de7bdcf81362f1ae628fd42bfb0b46
bluebirds2920 <110462756+bluebirds2920@users.noreply.github.com>
2024-01-27 19:40:35 +0700

    fix:filterbtnskill

--- components/SkillsCard.jsx
+++ components/SkillsCard.jsx
@@ -9,21 +9,6 @@ const SkillsCard = () => {
       <div className="text-xl font-bold">{data.name}</div>
     </div>
   ));
-
-  //   return (
-
-  //     <div className="w-[240px]  p-2 rounded-lg shadow-lg  bg-primary flex flex-col text-secondary items-center justify-center   ">
-  //       <div className="relative w-[120px] h-[120px] ">
-  //         <Image
-  //           src="/skill-icon/react.svg"
-  //           fill
-  //           priority
-  //           alt="Software-Developer-Image"
-  //         />
-  //       </div>
-  //       <div className="text-xl font-bold">React</div>
-  //     </div>
-  //   );
 };
 
 export default SkillsCard;
--- components/SkillsFilters.jsx
+++ components/SkillsFilters.jsx
@@ -5,20 +5,22 @@ const SkillsFilters = () => {
   const filterskills = ["All", "Languages", "Frameworks", "Databases", "Tools"];
 
   return (
-    <div className="flex flex-wrap   justify-center mt-6 mb-6 button-wraps">
-      <ToggleGroup size="sm" type="single">
-        {filterskills.map((item, index) => (
-          <ToggleGroupItem
-            className="bg-primary border-2 border-primary text-secondary"
-            value={index + 1}
-            key={index}
-            aria-label={item}
-          >
-            {item}
-          </ToggleGroupItem>
-        ))}
-      </ToggleGroup>
-    </div>
+    <ToggleGroup
+      className="flex flex-wrap justify-center mt-6 mb-6"
+      size="sm"
+      type="single"
+    >
+      {filterskills.map((item, index) => (
+        <ToggleGroupItem
+          className="bg-primary border-2 border-primary text-secondary"
+          value={index + 1}
+          key={index}
+          aria-label={item}
+        >
+          {item}
+        </ToggleGroupItem>
+      ))}
+    </ToggleGroup>
   );
 };
 
