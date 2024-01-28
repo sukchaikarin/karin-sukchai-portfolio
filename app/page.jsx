edba6f49757bfec86b9d5a057ea0344473fe77cd
bluebirds2920 <110462756+bluebirds2920@users.noreply.github.com>
2024-01-28 21:46:35 +0700

    what-is-404-contact

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
 // components
-import Contact from "@/components/Contact";
+
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-
+import Contact from "@/components/Contact";
 export default function Home() {
   return (
     <main>
